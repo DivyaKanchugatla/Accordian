3efe53a6636e7ce3a98277593a8065449b911894
DIVYA KANCHUGATLA <divyakanchugatla5@gmail.com>
2023-06-05 12:19:08 +0530

    pushed code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const data = [
+  {
+    q1:"How are you?",
+    a1:"I am good,thanks for asking"
+  },
+  {
+    q1:"Do you read newspapers?",
+    a1:"Yes,I do but not everyday"
+  },
+  {
+    q1:"Are you React developer",
+    a1:"Yes,I am"
+  },
+  {
+    q1:"Which you are working for?",
+    a1:"Msys Technologies PVT LTD"
+  }
+]
+
 function App() {
+  const  [selected,setSelected] = useState(null);
+  const toggle = (i) => {
+    if(selected===i){
+      return setSelected(null)
+    }
+      setSelected(i)
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className='accordian'>
+       {data.map((each,i)=>{
+        return (
+          <div className='items' key={i} onClick={()=>toggle(i)}>
+            <div className='title'>
+            <h3>{each.q1}</h3>
+            <span>{selected===i ? "-" : "+"}</span>
+            </div>
+             
+             <p className={selected===i ? "content show" : "content"}>{each.a1}</p>
+           </div>
+        )
+       })}
+      </div>
     </div>
   );
 }
